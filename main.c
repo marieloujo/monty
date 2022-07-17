daeef16f0d2aa96bf45c65c309874cb20c3f6aed
marieloujo <detchenoujoan@gmail.com>
2022-07-17 22:12:47 +0100

    Initial commit

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "monty.h"
+
+/**
+ * main - monty main program
+ * @argc: number of argument passed
+ * @argv: list of arguments
+ * Return: Nothing
+ */
+void main(int argc, char *argv[])
+{
+
+}
